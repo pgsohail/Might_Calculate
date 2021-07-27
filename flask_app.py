a68ad20309fb6831afd890001e6291dcc3f93494
張之耀(Chih-Yao, Chang) <m900054@gmail.com>
2021-07-27 23:06:14 +0800

    test

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
-@app.route("/app")
-@app.route("/test")
+@app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/test/')
+def test():
+    datas = ['1','2','3']
+    return render_template('index.html', datas = datas)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
