bc5db2f662ce166ae6f6d726c4506123f26c7bd3
張之耀(Chih-Yao, Chang) <m900054@gmail.com>
2023-11-13 16:24:16 +0800

    new certificate
    新增: 付費等級, 金鑰授權到期日期

--- flask_app.py
+++ flask_app.py
@@ -23,6 +23,8 @@ class Variables:
         self.PASS_MSG =          'CERTIFICATE_KEY_CHECK_PASSED____'
         self.FAIL_MSG =          'CERTIFICATE_KEY_CHECK_FAILED____'
         self.VERSION_ERROR_MSG = 'VERSION_CHECK_ERROR_____________'
+        self.EXPIRATION_DATE =   ''                           # 20231019_18  代表 2023/10/19 18:00:00 到期
+        self.PRICE_LEVEL =       ''
 
     def get_now_key(self, now_time, retry=0):
         if retry == 0:
@@ -68,7 +70,7 @@ def encrypt_data(data:str) -> str:
 def decrypt_data(encrypt_json_str:str) -> str:
     GMT8_time = datetime.utcnow() + timedelta(hours=8)
     for i in range(3):
-        now_key = my_var.get_now_key(now_time=GMT8_time, retry=i)            
+        now_key = my_var.get_now_key(now_time=GMT8_time, retry=i)
         try:
             encrypt_dict = json.loads(encrypt_json_str)
             cipher_text = b64decode(encrypt_dict['1'])
@@ -88,7 +90,7 @@ def decrypt_data(encrypt_json_str:str) -> str:
 # @app.route("/")
 # def hello_world():
 #     return "<p>Hello, World!</p>"
-    
+
 @app.route("/gta")
 def gta():
     return "<p>gta key</p>"
@@ -102,91 +104,13 @@ def travel():
         GMT8_time = time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime(t1))
         with open(dir_+'login_travel.txt', 'a') as f_write:
             f_write.write(GMT8_time + '\n')
-    
+
     return render_template("travel.html")
 
 @app.route('/form')
 def formPage():
     return render_template('form.html')
 
-'''
-@app.route('/submit', methods=['POST'])
-def submit():
-    #print(request.form)                 # ImmutableMultiDict([('user', '1027f5a0ad77'), ('status', 'login')])
-    #print(type(request.form))           # <class 'werkzeug.datastructures.ImmutableMultiDict'>
-    
-    my_dict = request.form.to_dict()
-    #print(my_dict)
-    
-    FLAG = 'False'
-    dir_ = 'Might_Calculate/'
-    
-    
-    if 'user' in my_dict.keys():
-        user = my_dict['user']
-        #print("post : user => ", user)
-    else:
-        user = 'None'
-        #print('no user key')
-        
-    if 'status' in my_dict.keys():
-        status = my_dict['status']
-        #print("post : status => ", status)
-    else:
-        status = 'None'
-        #print('no status key')
-        
-    if 'version' in my_dict.keys():
-        version = my_dict['version']
-        #print("post : version => ", version)
-    else:
-        version = 'None'
-        #print('no version key')
-        
-    with open(dir_+'valid_version.txt', 'r') as f:
-        valid_version_lines = f.readlines()
-        
-    version_check = False
-    for line in valid_version_lines:
-        if version == line.strip():
-            version_check = True
-        
-    if version_check == False:
-        print('version: ' + version)
-        return redirect(url_for('check', FLAG='version_error'))
-        
-    print(f'user: {user}, status: {status}, version: {version}')
-    
-    
-    
-    # if user == '1027f5a0ad77':
-        # FLAG = 'True'
-        
-    
-    if user != '1027f5a0ad77':
-        t0 = time.time()
-        t1 = t0 + 60*60*8   # GMT +8
-        GMT8_time = time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime(t1))
-        with open(dir_+'login.txt', 'a') as f_write:
-            f_write.write(user + ' | ' + GMT8_time + ' | ' + status)
-            f_write.write('\n')
-    
-    with open(dir_+'mac.txt', 'r') as f:
-        lines = f.readlines()
-    for item in lines:
-        if user == item.split('\n')[0]:
-            FLAG = 'True'
-            break
-            
-    if FLAG == 'False':
-        with open(dir_+'new_user.txt', 'a') as f_write:
-            f_write.write(user)
-            f_write.write('\n')
-        # FLAG = 'True'        # 先開給Win11使用者使用，因為win11重新開機後，mac addr都會一直變
-    
-    # time.sleep(2)
-    return redirect(url_for('check', FLAG=FLAG))
-'''
 
 @app.route('/certificate_key_GTA_Auto_Play', methods=['POST'])
 def certificate_key_GTA_Auto_Play():
@@ -194,7 +118,7 @@ def certificate_key_GTA_Auto_Play():
         json_str = request.get_json()
     else:
         abort(404)
-    
+
     decrypt_json_str = decrypt_data(json_str)
     # print(decrypt_json_str, flush=True)
     if decrypt_json_str == None:
@@ -210,37 +134,42 @@ def certificate_key_GTA_Auto_Play():
         version_dict = json.load(f)
 
     FLAG = my_var.FAIL_MSG
-    
+
     login_file = 'login_GTA_Auto_Play.txt'
     mac_file = 'mac_GTA_Auto_Play.txt'
     new_user_file = 'new_user_GTA_Auto_Play.txt'
     valid_version_list = version_dict['GTA_Auto_Play']['VALID_VERSION']
-    
-    
+
+
     if 'user' in my_dict.keys():
         user = my_dict['user']
         #print("post : user => ", user)
     else:
         user = 'None'
         #print('no user key')
-        
+
     if 'status' in my_dict.keys():
         status = my_dict['status']
         #print("post : status => ", status)
     else:
         status = 'None'
         #print('no status key')
-        
+
     if 'version' in my_dict.keys():
         version = my_dict['version']
         #print("post : version => ", version)
     else:
         version = 'None'
         #print('no version key')
-        
-    
-        
-    
+
+
+    if 'new_certificate_key' in my_dict.keys():
+        new_certificate_key = my_dict['new_certificate_key']
+    else:
+        new_certificate_key = 'None'
+
+
+
     if version in valid_version_list:
         pass
     else:
@@ -249,10 +178,11 @@ def certificate_key_GTA_Auto_Play():
         # return redirect(url_for('check', FLAG=my_var.VERSION_ERROR_MSG))
         FLAG = my_var.VERSION_ERROR_MSG
         return encrypt_data(FLAG)
-    
-    print(f'user: {user}, status: {status}, version: {version}')
-    
-    
+
+    # print(f'user: {user}, status: {status}, version: {version}, new_certificate_key: {new_certificate_key}')
+    print(f'status: {status:14}, ver: {version:12}, user: {user}')
+
+
     if user != '1027f5a0ad77':
         t0 = time.time()
         t1 = t0 + 60*60*8   # GMT +8
@@ -260,20 +190,20 @@ def certificate_key_GTA_Auto_Play():
         with open(root_dir + login_file, 'a') as f_write:
             f_write.write(user + ' | ' + GMT8_time + ' | ' + status)
             f_write.write('\n')
-    
+
     with open(root_dir + mac_file, 'r') as f:
         lines = f.readlines()
     for item in lines:
         if user == item.split('\n')[0]:
             FLAG = my_var.PASS_MSG
             break
-            
+
     if FLAG == my_var.FAIL_MSG:
         with open(root_dir + new_user_file, 'a') as f_write:
             f_write.write(user)
             f_write.write('\n')
         # FLAG = my_var.PASS_MSG        # 先開給Win11使用者使用，因為win11重新開機後，mac addr都會一直變
-    
+
     # time.sleep(2)
     # return redirect(url_for('check', FLAG=FLAG))
     return encrypt_data(FLAG)
@@ -285,7 +215,7 @@ def certificate_key_animal_restaurant():
         json_str = request.get_json()
     else:
         abort(404)
-    
+
     decrypt_json_str = decrypt_data(json_str)
     # print(decrypt_json_str, flush=True)
     if decrypt_json_str == None:
@@ -301,37 +231,42 @@ def certificate_key_animal_restaurant():
         version_dict = json.load(f)
 
     FLAG = my_var.FAIL_MSG
-    
+
     login_file = 'login_animal.txt'
     mac_file = 'mac_animal.txt'
     new_user_file = 'new_user_animal.txt'
     valid_version_list = version_dict['Animal_Restaurant']['VALID_VERSION']
-    
-    
+
+
     if 'user' in my_dict.keys():
         user = my_dict['user']
         #print("post : user => ", user)
     else:
         user = 'None'
         #print('no user key')
-        
+
     if 'status' in my_dict.keys():
         status = my_dict['status']
         #print("post : status => ", status)
     else:
         status = 'None'
         #print('no status key')
-        
+
     if 'version' in my_dict.keys():
         version = my_dict['version']
         #print("post : version => ", version)
     else:
         version = 'None'
         #print('no version key')
-        
-    
-        
-    
+
+    # if 'new_certificate_key' in my_dict.keys():
+    #     new_certificate_key = my_dict['new_certificate_key']
+    # else:
+    #     new_certificate_key = 'None'
+
+
+
+
     if version in valid_version_list:
         pass
     else:
@@ -340,10 +275,11 @@ def certificate_key_animal_restaurant():
         # return redirect(url_for('check', FLAG=my_var.VERSION_ERROR_MSG))
         FLAG = my_var.VERSION_ERROR_MSG
         return encrypt_data(FLAG)
-    
-    print(f'user: {user}, status: {status}, version: {version}')
-    
-    
+
+    # print(f'user: {user}, status: {status}, ver: {version}, new_certificate_key: {new_certificate_key}')
+    print(f'status: {status:14}, ver: {version:12}, user: {user}')
+
+
     if user != '1027f5a0ad77':
         t0 = time.time()
         t1 = t0 + 60*60*8   # GMT +8
@@ -351,24 +287,124 @@ def certificate_key_animal_restaurant():
         with open(root_dir + login_file, 'a') as f_write:
             f_write.write(user + ' | ' + GMT8_time + ' | ' + status)
             f_write.write('\n')
-    
+
+    with open(root_dir + mac_file, 'r') as f:
+        lines = f.readlines()
+    for line in lines:
+        pure_line = line.strip()
+        if pure_line.find(user) != -1:
+            EXPIRATION_DATE = pure_line.split(',')[1]
+            PRICE_LEVEL = pure_line.split(',')[2]
+            FLAG = f'{my_var.PASS_MSG},{EXPIRATION_DATE},{PRICE_LEVEL}'
+            break
+
+    if FLAG == my_var.FAIL_MSG:
+        with open(root_dir + new_user_file, 'a') as f_write:
+            f_write.write(user)
+            f_write.write('\n')
+        # FLAG = my_var.PASS_MSG        # 先開給Win11使用者使用，因為win11重新開機後，mac addr都會一直變
+
+
+    # time.sleep(2)
+    # return redirect(url_for('check', FLAG=FLAG))
+    return encrypt_data(FLAG)
+
+
+@app.route('/certificate_key_HarryPotter_Magic_Awakened', methods=['POST'])
+def certificate_key_HarryPotter_Magic_Awakened():
+    if request.is_json:
+        json_str = request.get_json()
+    else:
+        abort(404)
+
+    decrypt_json_str = decrypt_data(json_str)
+    # print(decrypt_json_str, flush=True)
+    if decrypt_json_str == None:
+        abort(404)
+
+    try:
+        my_dict = json.loads(decrypt_json_str)
+        # print(my_dict, flush=True)
+    except:
+        abort(404)
+
+    with open(VALID_VERSION_AND_DOWNLOAD_FILE, 'r', encoding='utf-8') as f:
+        version_dict = json.load(f)
+
+    FLAG = my_var.FAIL_MSG
+
+    login_file = 'login_HarryPotter_Magic_Awakened.txt'
+    mac_file = 'mac_HarryPotter_Magic_Awakened.txt'
+    new_user_file = 'new_user_HarryPotter_Magic_Awakened.txt'
+    valid_version_list = version_dict['HarryPotter_Magic_Awakened']['VALID_VERSION']
+
+
+    if 'user' in my_dict.keys():
+        user = my_dict['user']
+        #print("post : user => ", user)
+    else:
+        user = 'None'
+        #print('no user key')
+
+    if 'status' in my_dict.keys():
+        status = my_dict['status']
+        #print("post : status => ", status)
+    else:
+        status = 'None'
+        #print('no status key')
+
+    if 'version' in my_dict.keys():
+        version = my_dict['version']
+        #print("post : version => ", version)
+    else:
+        version = 'None'
+        #print('no version key')
+
+    # if 'new_certificate_key' in my_dict.keys():
+    #     new_certificate_key = my_dict['new_certificate_key']
+    # else:
+    #     new_certificate_key = 'None'
+
+
+
+
+    if version in valid_version_list:
+        pass
+    else:
+        # version_check = False
+        print(f'version_check FAILED, version: {version}')
+        # return redirect(url_for('check', FLAG=my_var.VERSION_ERROR_MSG))
+        FLAG = my_var.VERSION_ERROR_MSG
+        return encrypt_data(FLAG)
+
+    # print(f'user: {user}, status: {status}, ver: {version}, new_certificate_key: {new_certificate_key}')
+    print(f'status: {status:14}, ver: {version:12}, user: {user}')
+
+
+    if user != '1027f5a0ad77':
+        t0 = time.time()
+        t1 = t0 + 60*60*8   # GMT +8
+        GMT8_time = time.strftime("%Y/%m/%d %H:%M:%S", time.gmtime(t1))
+        with open(root_dir + login_file, 'a') as f_write:
+            f_write.write(user + ' | ' + GMT8_time + ' | ' + status)
+            f_write.write('\n')
+
     with open(root_dir + mac_file, 'r') as f:
         lines = f.readlines()
     for item in lines:
         if user == item.split('\n')[0]:
             FLAG = my_var.PASS_MSG
             break
-            
+
     if FLAG == my_var.FAIL_MSG:
         with open(root_dir + new_user_file, 'a') as f_write:
             f_write.write(user)
             f_write.write('\n')
         # FLAG = my_var.PASS_MSG        # 先開給Win11使用者使用，因為win11重新開機後，mac addr都會一直變
-    
+
     # time.sleep(2)
     # return redirect(url_for('check', FLAG=FLAG))
     return encrypt_data(FLAG)
-
 
 
 @app.route('/check/<FLAG>')
@@ -428,7 +464,7 @@ def test(id):
         #     'file_name':file_name
         # }
         weapons.append(file_name)
-    
+
     for file_name in os.listdir('Might_Calculate/static/img/dragon'):
         # name, hp, str, might = file_name.split('_')
         # might = might.split('.')[0]
